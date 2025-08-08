3bdac3d288b4f621fb8108be8b2549edb18e5ad9
Nicholas Eldergill <Nicholas.Eldergill@city.ac.uk>
2025-08-08 10:49:49 +0100

    Merge

--- index.js
+++ index.js
@@ -25,14 +25,10 @@ while(running){
         playerOne.typeOfChar = "O"
     }
 
-    editGrid(1,1,playerOne.typeOfChar)
-
-    console.log(grid);
-    console.log(grid[1][1]);
     answer = prompt("Choose your first X position! row column ")
     let x = answer.split(" ")[0]
     let y = answer.split(" ")[1]
-    editGrid(x, y, "X") 
+    editGrid(x, y, playerOne.typeOfChar) 
     console.log(grid)
     
 
